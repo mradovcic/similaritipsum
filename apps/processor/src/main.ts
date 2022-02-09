2eb679e6eb23f1b3b3d138e8cf2bd6f86ca722ef
mradovcic <radovcicmarko@gmail.com>
2022-02-09 22:37:42 +0100

    proccesor init, rabbit listener added

--- apps/processor/src/main.ts
+++ apps/processor/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ProcessorModule } from './processor.module';
+import { ProcessorService } from './processor.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ProcessorModule);
-  await app.listen(3000);
+  const app = await NestFactory.createApplicationContext(ProcessorModule);
+  const processorService = app.get(ProcessorService);
+  await processorService.start();
 }
 bootstrap();
--- apps/processor/src/processor.service.ts
+++ apps/processor/src/processor.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { RabbitMqHelper } from 'core/core/helpers/rabbitmq.helper';
 
 @Injectable()
 export class ProcessorService {
-  getHello(): string {
-    return 'Hello World!';
+  async start(): Promise<void> {
+    const connection = await RabbitMqHelper.createConnection('connection');
+    await RabbitMqHelper.initializeListener(
+      connection,
+      'Similaritypsum.Report',
+      'Similaritypsum.Exchange',
+      ['Similaritypsum.Report.Reques'],
+      async (msg) => {
+        console.log(msg);
+      },
+      1,
+    );
   }
 }
